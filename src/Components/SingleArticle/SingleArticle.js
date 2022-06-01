d78ea058f188b0b6371da14edac2659c4cc08467
Sam Mistele <smmstl0@gmail.com>
2022-06-01 17:17:47 -0600

    Add functionality for matching id on click

--- src/Components/SingleArticle/SingleArticle.js
+++ src/Components/SingleArticle/SingleArticle.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import './SingleArticle.css'
+import './SingleArticle.css';
+import SingleArticleView from '../SingleArticleView/SingleArticleView';
+import { Link } from 'react-router-dom';
 
-const SingleArticle = ({ section, title, abstract, multimedia }) => {
+const SingleArticle = ({ section, title, abstract, multimedia, id, setArticle }) => {
 
   return (
-    <div className='card'>
-      <img src={multimedia[2].url} alt={multimedia[2].section}/>
-      <h1>{title}</h1>
-      <p>{section}</p>
-      <p>{abstract}</p>
-    </div>
+    <Link to={`/article-view/${id}`}>
+      <div className='card' onClick={() => setArticle(id)}>
+        <img src={multimedia[2].url} alt={multimedia[2].section}/>
+        <h1>{title}</h1>
+        <p>{section}</p>
+      </div>
+    </Link>
   )
 }
 
