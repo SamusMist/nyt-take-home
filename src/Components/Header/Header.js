b0a4b03e19216b1524d8c00ad561a72caf9416e5
Sam Mistele <smmstl0@gmail.com>
2022-06-01 17:40:53 -0600

    Add header functionality

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+import homeIcon from '../../home-icon.js';
 
 const Header = () => {
 
+  return (
+    <div className='header'>
+      <h1>Turing Times</h1>
+      <Link to='/'>
+        <img className='home-icon' src={homeIcon}>
+      </Link>
+    </div>
+  )
 }
 
 export default Header;
