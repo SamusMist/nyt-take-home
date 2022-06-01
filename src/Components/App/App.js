b32b5cb4e7519bfb26861839c7d1f1135263d12e
Sam Mistele <smmstl0@gmail.com>
2022-06-01 11:16:09 -0600

    Add useEffect to App

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,15 +6,20 @@ import Error from '../../Error';
 import Home from '../../Home';
 
 const App = () => {
-  const [stories, setStories] = useState([])
+  const [stories, setStories] = useState([]);
+  const [error, setError] = useState('');
 
-const fetchStories = () => {
-  fetchData.getData(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=5tD1M6InGK2xFGKqj8uIHTo5EZJ6Yo0i`)
-     .then(data => setStories(data.results))
-     .catch((error) => {
-       setError(error)
-    })
-}
+  const fetchStories = () => {
+    fetchData.getData(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=5tD1M6InGK2xFGKqj8uIHTo5EZJ6Yo0i`)
+       .then(data => setStories(data.results))
+       .catch((error) => {
+         setError(error)
+      });
+  }
+
+  useEffect(() => {
+    fetchStories();
+  }, [])
 
   return (
     <div className="App">
